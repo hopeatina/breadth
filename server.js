b3751cad0c3500199b65632bcde205336590d417
hopeatina <hnatina@ITSM8009261.local>
2015-11-12 14:11:58 -0600

    remove require and correct port

--- server.js
+++ server.js
@@ -5,13 +5,12 @@ var mongoose = require('mongoose');                     // mongoose for mongodb
 var morgan = require('morgan');             // log requests to the console (express4)
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
-var routes = require('./routes');
 var books = require('./app/src/users/bookModel');
 var passport = require('passport');
 var expressSession = require('express-session');
 var cookieParser = require('cookie-parser');
 
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 // configuration =================
 
