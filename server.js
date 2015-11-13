079885db3ae3086114443b0e25ef4c9a2b6cab96
hopeatina <hnatina@ITSM8009261.local>
2015-11-12 19:24:43 -0600

    server routes try1

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ var mongoose = require('mongoose');                     // mongoose for mongodb
 var morgan = require('morgan');             // log requests to the console (express4)
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var routes = require('../routes');
 var books = require('./app/src/users/bookModel');
 var passport = require('passport');
 var expressSession = require('express-session');
 var cookieParser = require('cookie-parser');
 
-var port = process.env.PORT || 8080;
-
+app.set('port', process.env.PORT || 8080);
+var port = app.get('port')
 // configuration =================
 
 //mongoose.connect('mongodb://localhost/test'); // connect to mongoDB database local
